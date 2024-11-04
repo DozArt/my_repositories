e1103df307edcd13c71b9090cd90be8041a85fd4
DozArt <120102438+DozArt@users.noreply.github.com>
2024-11-04 12:04:01 +0300

    add methods

--- js_demonstration/methods.js
+++ js_demonstration/methods.js
@@ -1,59 +1,87 @@
 // Методы массивов, функций, объектов, коллекций
 let arr = [1, 2, 3, 4, 5];
 const arrConst = ["с", "ч", "а", "с", "т", "ь", "е"];
+
+let users = [
+  {id: 1, name: "Вася"},
+  {id: 2, name: "Петя"},
+  {id: 3, name: "Маша"},
+  {id: 4, name: "Вася"}
+];
+
 let valReturn = [];
 outputValue();
+
+function resetArr(){
+  arr = [1, 2, 3, 4, 5];
+  valReturn = [];
+  outputValue();
+}
 
 function createElement(id, array) {
   var destinationArr = document.getElementById(id);
   destinationArr.textContent = "";
-
-  array.forEach((item, index) => {
-    const divIndex = document.createElement("div");
-    divIndex.textContent = index;
-
+  if (!Array.isArray(array)){
     const span = document.createElement("span");
-
-    span.textContent = item;
-
+    span.textContent = JSON.stringify(array);
+    const divIndex = document.createElement("div");
+    divIndex.textContent = typeof array;
     destinationArr.appendChild(span);
     span.appendChild(divIndex);
-  });
+  }
+  else{
+    destinationArr.classList.add("items_array")
+    array.forEach((item, index) => {
+      const divIndex = document.createElement("div");
+      divIndex.textContent = index;
+
+      const span = document.createElement("span");
+
+      span.textContent = JSON.stringify(item);
+
+      destinationArr.appendChild(span);
+      span.appendChild(divIndex);
+    });
+  }
+  
 }
 
 function outputValue() {
   createElement("valueArr", arr);
   createElement("valueArrConst", arrConst);
   createElement("valueReturn", valReturn);
+  createElement("valueUsers", users);
+  valReturn = []
 }
 
 function methodArrPush() {
-  arr.push("new");
+  valReturn = arr.push("new");
   outputValue();
 }
 
 function methodArrPop() {
-  arr.pop();
+  valReturn = arr.pop();
+
   outputValue();
 }
 
 function methodArrShift() {
-  arr.shift();
+  valReturn = arr.shift();
   outputValue();
 }
 
 function methodArrUnshift() {
-  arr.unshift("new");
+  valReturn = arr.unshift("new");
   outputValue();
 }
 
 function methodArrSplise(...items) {
-  arr.splice(...items);
+  valReturn = arr.splice(...items);
   outputValue();
 }
 
-function methodArrSlice(start = 1, end = 6) {
-  valReturn = arrConst.slice(start, en);
+function methodArrSlice(...items) {
+  valReturn = arrConst.slice(...items);
   outputValue();
 }
 
@@ -63,18 +91,49 @@ function methodArrConcat() {
 }
 
 function methodArrForEach() {
-  arrConst.forEach((item, index) => console.log("iter", index, "->", item));
+  valReturn = arr.forEach((item,index) => {
+    arr[index] = item *2 ;
+  });
   outputValue();
 }
 
 function methodArrIndexOf() {
-  console.log('indexOf("с") ', arrConst.indexOf("с"));
-  console.log('lastIndexOf("с") ', arrConst.lastIndexOf("с"));
-  console.log('includes("с") ', arrConst.includes("с"));
+  valReturn = arrConst.indexOf("с");
   outputValue();
 }
-//find и findIndex/findLastIndex
-//filter
+function methodArrlastIndexOf() {
+  valReturn = arrConst.lastIndexOf("с");
+  outputValue();
+}
+function methodArrIncludes() {
+  valReturn = arrConst.includes("с");
+  outputValue();
+}
+
+function usersFind(){
+  valReturn = users.find(item => item.id == 1);
+  outputValue();
+}
+function usersFindIndex(){
+  valReturn = users.findIndex(item => item.name == "Вася");
+  outputValue();
+}
+function usersFindLastIndex(){
+  valReturn = users.findLastIndex(item => item.name == "Вася");
+  outputValue();
+}
+function usersFilter(){
+  valReturn = users.filter(item => item.id < 3);
+  outputValue();
+}
+function usersFilter(){
+  valReturn = users.filter(item => item.name == "Вася");
+  outputValue();
+}
+function usersMap(){
+  valReturn = users.map(item => item.name.toUpperCase());
+  outputValue();
+}
 //map
 //sort(fn)
 //reverse
